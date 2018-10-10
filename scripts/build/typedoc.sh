16537e8e7e1e8ff9dfad88d53e1f791983f5fcf0
Mike North <michael.l.north@gmail.com>
2018-10-09 23:27:35 -0700

    Typedoc

--- scripts/build/typedoc.sh
+++ scripts/build/typedoc.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
 echo "hello"
+source ./scripts/run_for_each.sh
+
+typedoc_for_example() {
+  ./node_modules/.bin/typedoc --name $1 --mode modules --module commonjs --target ES6 --excludePrivate --excludeNotExported --out docs/typedoc/$1 cases/$1
+}
+rm -rf docs/typedoc
+run_for_each_example typedoc_for_example
---
+++ scripts/run_for_each.sh
@@ -0,0 +1,17 @@
+run_for_each_example() {
+  run_for_example "1-single-file-js" "$@"
+  run_for_example "2-single-file-ts" "$@"
+  run_for_example "3-multi-file-js" "$@"
+  run_for_example "4-multi-file-ts" "$@"
+  run_for_example "5-ts-exported-types" "$@"
+  run_for_example "6-ts-advanced-types" "$@"
+  run_for_example "7-js-consumes-ts" "$@"
+  run_for_example "8-no-tsconfig" "$@"
+}
+run_for_example() {
+  proj=$1
+  fn=$2
+  echo "Running example: $proj"
+  $fn $proj
+}
+
