c1021f774843240f5b69af27022df2e88a1f62ab
Mike North <michael.l.north@gmail.com>
2018-10-10 14:47:13 -0700

    feat: docco

---
+++ scripts/build/docco.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+source ./scripts/run_for_each.sh
+
+docco_for_example() {
+  cd cases/$1
+  ../../node_modules/.bin/docco -o ../../docs/docco/$1 src/*.*s
+  cd ../..
+}
+rm -rf docs/docco
+run_for_each_example docco_for_example
--- scripts/build/docs.sh
+++ scripts/build/docs.sh
@@ -3,4 +3,6 @@ yarn build:esdoc
 yarn build:jsdoc
 yarn build:typedoc
 yarn build:ts-docs-gen
+yarn build:yuidoc
+yarn build:docco
 yarn build:index
