7df095124cf35ee522b4d761a9b66f70026e383a
Mike North <michael.l.north@gmail.com>
2018-10-15 21:01:29 -0700

    fix: rebuild JSdoc with readme

--- scripts/build/jsdoc.sh
+++ scripts/build/jsdoc.sh
@@ -5,7 +5,7 @@ source ./scripts/run_for_each.sh
 jsdoc_for_example() {
   cd cases/$1
   tsc --outDir ../../jsdoc-out/$1
-  ../../node_modules/.bin/jsdoc ../../jsdoc-out/$1 -d ../../docs/jsdoc/$1
+  ../../node_modules/.bin/jsdoc ../../jsdoc-out/$1 --readme ../../README.md -d ../../docs/jsdoc/$1
   cd ../..
 }
 rm -rf jsdoc-out
