8bd4c1e666ac877f9f39e9d3e50214d17b564b99
Mike North <michael.l.north@gmail.com>
2018-10-15 22:19:48 -0700

    fix: typedoc - add --allowJs flag

--- scripts/build/typedoc.sh
+++ scripts/build/typedoc.sh
@@ -3,7 +3,7 @@
 source ./scripts/run_for_each.sh
 
 typedoc_for_example() {
-  ./node_modules/.bin/typedoc --checkJs --noEmit --allowJs --ignoreCompilerErrors --name $1 --mode modules --module amd --target ES6 --out docs/typedoc/$1 cases/$1/src
+  ./node_modules/.bin/typedoc --allowJs --checkJs --noEmit --allowJs --ignoreCompilerErrors --name $1 --mode modules --module amd --target ES6 --out docs/typedoc/$1 cases/$1/src
 }
 rm -rf docs/typedoc
 run_for_each_example typedoc_for_example
